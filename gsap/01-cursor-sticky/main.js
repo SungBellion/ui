eafe14b3552c32b6acb56fb62865a1572eb21265
SungBellion <sung.bellion@hotmail.com>
2024-09-07 12:56:18 +0200

    Update sample 01

--- gsap/01-cursor-sticky/main.js
+++ gsap/01-cursor-sticky/main.js
@@ -1,3 +1,30 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log("OK");
-});
+function initCustomCursor() {
+    gsap.set('#product-display span.stickyCursor', { yPercent: -50, xPercent: -50 });
+
+    let activeImage;
+    gsap.utils.toArray("#product-display").forEach((el) => {
+        let image = el.querySelector('span.stickyCursor'),
+            setX, setY,
+            align = e => {
+                setX(e.clientX);
+                setY(e.clientY);
+            },
+            startFollow = () => document.addEventListener("mousemove", align),
+            stopFollow = () => document.removeEventListener("mousemove", align),
+            fade = gsap.to(image, {autoAlpha: 1, ease: "none", paused: true, onReverseComplete: stopFollow});
+        
+        el.addEventListener('mouseenter', (e) => {
+            fade.play();
+            startFollow();
+            if (activeImage) {
+                gsap.set(image, {x: gsap.getProperty(activeImage, "x"), y: gsap.getProperty(activeImage, "y")});
+            }
+            activeImage = image;
+            setX = gsap.quickTo(image, "x", {duration: 0.6, ease: "power3"}),
+            setY = gsap.quickTo(image, "y", {duration: 0.6, ease: "power3"})
+            align(e);
+        });
+        
+        el.addEventListener('mouseleave', () => fade.reverse());
+    });
+}
