6f9ebfba5867d3286b0d54305f2a972a54b22c87
SungBellion <sung.bellion@hotmail.com>
2024-10-04 14:38:43 +0200

    Create Odometer Counter 02

---
+++ gsap/02-odometer-counter/main.js
@@ -0,0 +1,33 @@
+let currentValue = "20000";
+
+function animateOdometer(target, start, end, duration) {
+  const startY = -(100 * start);
+  const endY = -(100 * end);
+  gsap.fromTo(target, { y: startY }, { y: endY, duration: duration, ease: "power2.out" });
+}
+
+function startOdometerAnimation(newValue) {
+  const currentDigits = currentValue.padStart(5, "0").split("");
+  const newDigits = newValue.padStart(5, "0").split("");
+
+  for (let i = 0; i < 5; i++) {
+    const currentDigit = parseInt(currentDigits[i], 10);
+    const newDigit = parseInt(newDigits[i], 10);
+
+    if (currentDigit !== newDigit) {
+      const target = `#digit-${5 - i}`;
+      animateOdometer(target, currentDigit, newDigit, 0.5);
+    }
+  }
+
+  currentValue = newValue;
+}
+
+document.querySelectorAll('button[data-number]').forEach(button => {
+  button.addEventListener('click', function () {
+    const newValue = this.getAttribute('data-number');
+    if (newValue !== currentValue) {
+      startOdometerAnimation(newValue);
+    }
+  });
+});
